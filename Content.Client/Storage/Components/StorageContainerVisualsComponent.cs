96e2d30739b4240978722343d9838e7212726f42
Prole <172158352+Prole0@users.noreply.github.com>
2026-06-15 23:45:42 -0700

    Import Commit

--- Content.Client/Storage/Components/StorageContainerVisualsComponent.cs
+++ Content.Client/Storage/Components/StorageContainerVisualsComponent.cs
@@ -6,19 +6,32 @@ namespace Content.Client.Storage.Components;
 /// <summary>
 ///     Essentially a version of <see cref="SolutionContainerVisualsComponent"/> fill level handling but for item storage.
 ///     Depending on the fraction of storage that's filled, will change the sprite at <see cref="FillLayer"/> to the nearest
-///     fill level, up to <see cref="MaxFillLevels"/>.
+///     fill level, up to <see cref="MaxFillLevels"/>. Does the same for the Equipped/InHands versions but in their respective
+///     code, OnGetHeldVisuals for InHands & OnGetClothingVisuals for Equipped.
 /// </summary>
 [RegisterComponent]
 public sealed partial class StorageContainerVisualsComponent : Component
 {
-    [DataField("maxFillLevels")]
-    public int MaxFillLevels = 0;
+    [DataField]
+    public int MaxFillLevels;
+
+    [DataField]
+    public int InHandsMaxFillLevels;
+
+    [DataField]
+    public int EquippedMaxFillLevels;
 
     /// <summary>
     ///     A prefix to use for the fill states., i.e. {FillBaseName}{fill level} for the state
     /// </summary>
-    [DataField("fillBaseName")]
+    [DataField]
     public string? FillBaseName;
+
+    [DataField]
+    public string? InHandsFillBaseName;
+
+    [DataField]
+    public string? EquippedFillBaseName;
 
     [DataField("layer")]
     public StorageContainerVisualLayers FillLayer = StorageContainerVisualLayers.Fill;
