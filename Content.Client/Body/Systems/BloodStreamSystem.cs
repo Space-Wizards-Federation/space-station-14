215381057326ba47f0e3a11858cffd3190f08930
Prole <172158352+Prole0@users.noreply.github.com>
2026-06-17 20:05:28 -0700

    SharedBloodstreamSystem is a partial class. So it seems alright to label it the same.

--- Content.Client/Body/Systems/BloodStreamSystem.cs
+++ Content.Client/Body/Systems/BloodStreamSystem.cs
@@ -2,4 +2,4 @@ using Content.Shared.Body.Systems;
 
 namespace Content.Client.Body.Systems;
 
-public sealed class BloodStreamSystem : SharedBloodstreamSystem;
+public sealed partial class BloodStreamSystem : SharedBloodstreamSystem;
--- Content.Server/Body/Systems/BloodstreamSystem.cs
+++ Content.Server/Body/Systems/BloodstreamSystem.cs
@@ -2,4 +2,4 @@ using Content.Shared.Body.Systems;
 
 namespace Content.Server.Body.Systems;
 
-public sealed class BloodstreamSystem : SharedBloodstreamSystem;
+public sealed partial class BloodstreamSystem : SharedBloodstreamSystem;
