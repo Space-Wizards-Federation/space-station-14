fbea50bcf9e986025cc85c9351adb8143887eaf3
Whatstone <whatston3@gmail.com>
2026-06-18 22:39:47 -0400

    Remove "ProtoMan" instances

--- Content.Server/Atmos/EntitySystems/AtmosphereSystem.Gases.cs
+++ Content.Server/Atmos/EntitySystems/AtmosphereSystem.Gases.cs
@@ -12,7 +12,6 @@ namespace Content.Server.Atmos.EntitySystems
 {
     public sealed partial class AtmosphereSystem
     {
-        [Dependency] private IPrototypeManager _protoMan = default!;
 
         private GasReactionPrototype[] _gasReactions = [];
 
@@ -25,7 +24,7 @@ namespace Content.Server.Atmos.EntitySystems
         {
             base.InitializeGases();
 
-            _gasReactions = _protoMan.EnumeratePrototypes<GasReactionPrototype>().ToArray();
+            _gasReactions = ProtoManager.EnumeratePrototypes<GasReactionPrototype>().ToArray();
             Array.Sort(_gasReactions, (a, b) => b.Priority.CompareTo(a.Priority));
         }
 
