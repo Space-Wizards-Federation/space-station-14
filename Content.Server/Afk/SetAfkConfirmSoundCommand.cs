a63ba1e070d0022c2074d44bb8c838eb63e22bb0
metalgearsloth <comedian_vs_clown@hotmail.com>
2026-06-21 19:18:35 +1000

    wa

--- Content.Server/Afk/SetAfkConfirmSoundCommand.cs
+++ Content.Server/Afk/SetAfkConfirmSoundCommand.cs
@@ -3,6 +3,8 @@ using Content.Shared.Administration;
 using Content.Shared.CCVar;
 using Robust.Shared.Configuration;
 using Robust.Shared.Console;
+using Robust.Shared.ContentPack;
+using Robust.Shared.Prototypes;
 using Robust.Shared.Utility;
 
 namespace Content.Server.Afk;
@@ -11,6 +13,8 @@ namespace Content.Server.Afk;
 public sealed partial class SetAfkConfirmSoundCommand : LocalizedEntityCommands
 {
     [Dependency] private IConfigurationManager _cfg = default!;
+    [Dependency] private IPrototypeManager _proto = default!;
+    [Dependency] private IResourceManager _res = default!;
 
     public override string Command => "setafkconfirmationsound";
 
@@ -35,8 +39,10 @@ public sealed partial class SetAfkConfirmSoundCommand : LocalizedEntityCommands
 
     public override CompletionResult GetCompletion(IConsoleShell shell, string[] args)
     {
-        return args.Length == 1
-            ? CompletionResult.FromHint(Loc.GetString("cmd-setafkconfirmationsound-hint"))
-            : CompletionResult.Empty;
+        if (args.Length != 1)
+            return CompletionResult.Empty;
+
+        var options = CompletionHelper.AudioFilePath(args[0], _proto, _res);
+        return CompletionResult.FromHintOptions(options, Loc.GetString("cmd-setafkconfirmationsound-hint"));
     }
 }
